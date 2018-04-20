5a0250fe70db368f0297c42ccc69d011fbadd58a
Travis <travishn@uci.edu>
2018-04-19 23:36:58 -0700

    Created enqueue, dequeue, and show methods

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,17 +1,19 @@
 class Queue
   attr_reader :queue
-  
+
   def initialize
     @queue = []
   end
 
   def enqueue(el)
+    queue << el
   end
 
   def dequeue
+    queue.shift
   end
 
   def show
-
+    queue.dup
   end
 end
