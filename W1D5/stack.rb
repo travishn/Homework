7a06c12434e333f2805145a2ec8426792697a94a
Travis <travishn@uci.edu>
2018-04-19 23:31:06 -0700

    Created add, remove, and show methods

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,19 +1,19 @@
 class Stack
   attr_reader :stack
-  
+
   def initialize
     @stack = []
   end
 
   def add(el)
-    # adds an element to the stack
+    stack << el
   end
 
   def remove
-    # removes one element from the stack
+    stack.pop
   end
 
   def show
-    # return a copy of the stack
+    stack.dup
   end
 end
